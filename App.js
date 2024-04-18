9ceca0a5268e2d4eeae26b1216b784435c3ee8c5
manansethi22 <manansethi2002@gmail.com>
2024-04-18 12:27:51 +0530

    1st coomit

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  useCameraPermission,
+  useCameraDevice,
+  Camera,
+} from "react-native-vision-camera";
 
 export default function App() {
+  const { hasPermission, requestPermission } = useCameraPermission();
+
+  const device = useCameraDevice('back');
+
+  useEffect(() => {
+    if (!hasPermission) {
+      requestPermission();
+    }
+  }, [hasPermission]);
+
+  if (!hasPermission) {
+    return <Text>No access to camera. Turn it on from Settings</Text>;
+  }
+
+  if (!device) {
+    return <Text>Camera device not found</Text>;
+  }
+
+  console.log("has permisions: ", hasPermission);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+
       <StatusBar style="auto" />
+      <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
     </View>
   );
 }
@@ -13,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
